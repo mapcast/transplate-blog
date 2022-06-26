c19038808cdc68a6001b6153865bfed5c4d64a29
mapcast <odri0220@naver.com>
2022-06-26 12:27:57 +0900

    git init

---
+++ src/main/java/com/transplate/project/ReactblogTransplateApplication.java
@@ -0,0 +1,13 @@
+package com.transplate.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactblogTransplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactblogTransplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transplate/project/ReactblogTransplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transplate.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactblogTransplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
